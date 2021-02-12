56e43fb4623ba21d946ed03582715808ca513b60
Stewart Smith <stewart.dryden.smith@gmail.com>
2021-02-12 17:29:12 -0500

    Add carray_size

---
+++ test/carray_test.cpp
@@ -0,0 +1,14 @@
+#include "gtest/gtest.h"
+
+#include "carray.h"
+
+TEST(C_Array, size) {
+    char const a[1] = {};
+    EXPECT_EQ(sds::carray_size(a), 1U);
+
+    char const b[] = {1, 2, 3, 4, 5, 6};
+    EXPECT_EQ(sds::carray_size(b), 6U);
+
+    char const c[10] = {};
+    EXPECT_EQ(sds::carray_size(c), 10U);
+}
