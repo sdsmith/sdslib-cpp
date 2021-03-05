8ea1c973f61541093790c031605a13064ec1fc14
Stewart Smith <stewart.dryden.smith@gmail.com>
2021-03-04 20:55:28 -0500

    Add docs

--- include/sdslib/array/make_array.h
+++ include/sdslib/array/make_array.h
@@ -5,14 +5,17 @@
 
 namespace sds
 {
-
-// ref: https://gist.github.com/klmr/2775736#file-make_array-hpp
+/**
+ * \brief Make \a std::array from the given values.
+ *
+ * Avoids having to specify the number of elements in the array.
+ *
+ * ref: https://gist.github.com/klmr/2775736#file-make_array-hpp
+ */
 template <typename... T>
-constexpr auto make_array(T&&... values) ->
-    std::array<
-    typename std::decay<
-    typename std::common_type<T...>::type>::type,
-        sizeof...(T)> {
+constexpr auto make_array(T&&... values)
+    -> std::array<typename std::decay<typename std::common_type<T...>::type>::type, sizeof...(T)>
+{
     return {std::forward<T>(values)...};
 }
 
