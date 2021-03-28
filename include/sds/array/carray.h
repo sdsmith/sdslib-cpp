727166d3ff29afa71d194faf58cc43e8a31de279
Stewart Smith <stewart.dryden.smith@gmail.com>
2021-03-28 19:46:05 -0400

    byte size of carray

--- include/sds/array/carray.h
+++ include/sds/array/carray.h
@@ -11,4 +11,13 @@ namespace sds
     constexpr size_t carray_size(T (&)[N]) {
         return N;
     }
+
+    /**
+     * \brief Byte size of c-style array.
+     */
+    template<typename T, size_t N>
+    constexpr size_t byte_size(T (&)[N]) {
+        return N * sizeof(T);
+    }
+
 } // namespace sds
