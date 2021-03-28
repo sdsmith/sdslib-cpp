27ff3c40c48e65f0eff03461890220d3502976eb
Stewart Smith <stewart.dryden.smith@gmail.com>
2021-03-28 19:46:46 -0400

    Add str_size, the string size in code points

--- include/sds/string.h
+++ include/sds/string.h
@@ -8,11 +8,31 @@ constexpr char const* to_string(bool v) noexcept {
 }
 
 /**
- * Byte size of the contents of a string. Includes the null-terminator in the
- * count.
+ * \brief Byte size of the contents of a string. Includes the null-terminator in the count.
  *
  * \param s Null-terminated string.
  */
 int byte_size(char const* s) noexcept;
+
+/**
+ * \brief Number of elements in the string. Does not include the
+ * null-terminator.
+ *
+ * \tparam CodeUnit Type of the code unit in the string encoding.
+ * \param s Null-terminated string.
+ */
+template<typename CodeUnit>
+int str_size(CodeUnit const* s) noexcept {
+    if (!s) { return 0; }
+
+    CodeUnit const* p = s;
+    int size = 0;
+    while (*p != '\0') {
+        ++size;
+        ++p;
+    }
+
+    return size;
+}
 
 } // namespace sds
