e32a8c46ba9fd6a0e6a0d6e09a204a9c439edd53
Stewart Smith <stewart.dryden.smith@gmail.com>
2021-04-27 21:41:03 -0400

    add lib config to common + define lib's static assert

--- include/sds/details/common.h
+++ include/sds/details/common.h
@@ -1,13 +1,24 @@
 #pragma once
 
+#include "sds/config.h"
+
 #ifndef SDS_ASSERT
-#   include <cassert>
+#    include <cassert>
 /**
  * \def SDS_ASSERT
- * \brief Assert used by the library. Defualts to using cassert. Define \a
+ * \brief Assert used by the library. Defaults to using cassert. Define \a
  * SDS_ASSERT to use a custom assertion.
  */
-#   define SDS_ASSERT(x) assert(x)
+#    define SDS_ASSERT(x) assert(x)
+#endif
+
+#ifndef SDS_STATIC_ASSERT
+/**
+ * \def SDS_STATIC_ASSERT
+ * \brief Assert used by the library. Defaults to \a static_assert.
+ */
+#    define SDS_STATIC_ASSERT(x) static_assert(x)
+#    define SDS_STATIC_ASSERT_MSG(x, msg) static_assert(x, msg)
 #endif
 
 #include "sds/details/types.h"
