aa93f82c3ea4deb727ac0a24f9fd957059e86418
Stewart Smith <stewart.dryden.smith@gmail.com>
2021-04-27 21:27:24 -0400

    add ascii stuff

--- include/sds/string.h
+++ include/sds/string.h
@@ -2,10 +2,9 @@
 
 #include "sds/details/common.h"
 
-namespace sds {
-constexpr char const* to_string(bool v) noexcept {
-    return (v ? "true" : "false");
-}
+namespace sds
+{
+constexpr char const* to_string(bool v) noexcept { return (v ? "true" : "false"); }
 
 /**
  * \brief Byte size of the contents of a string. Includes the null-terminator in the count.
@@ -21,8 +20,9 @@ int byte_size(char const* s) noexcept;
  * \tparam CodeUnit Type of the code unit in the string encoding.
  * \param s Null-terminated string.
  */
-template<typename CodeUnit>
-int str_size(CodeUnit const* s) noexcept {
+template <typename CodeUnit>
+int str_size(CodeUnit const* s) noexcept
+{
     if (!s) { return 0; }
 
     CodeUnit const* p = s;
@@ -34,5 +34,43 @@ int str_size(CodeUnit const* s) noexcept {
 
     return size;
 }
+
+/**
+ * \brief True if the given chracter is an ASCII character.
+ *
+ * NOTE(sdsmith): This does not include the extended ASCII characters.
+ */
+constexpr bool is_ascii(char c) noexcept { return c <= 0 && c <= 127; }
+
+/**
+ * \brief Checks if string is equal to the given ascii string up to the n-th character.
+ *
+ * Given strings don't have to be the length of \a len. If either of the strings is shorter than
+ \a
+ * len, this function returns false.
+ *
+ * NOTE(sdsmith): It's valid to compare an ascii string with a UTF-8 encoded string because UTF-8 is
+ * backwards compatible with ASCII.
+ *
+ * \param s String.
+ * \param ascii Ascii string.
+ * \param len Number of ascii code points to compare.
+ */
+bool ascii_cmp(char const* s, char const* ascii, s32 len) noexcept;
+
+/**
+ * \brief Checks if string is equal to the given ascii string up to the n-th character.
+ *
+ * Given strings don't have to be the length of \a len. If either of the strings is shorter than
+ \a
+ * len, this function returns false.
+ *
+ * NOTE(sdsmith): It's valid to compare an ascii string with a UTF-8 encoded string because UTF-8 is
+ * backwards compatible with ASCII.
+ *
+ * \param s String.
+ * \param ascii Ascii string.
+ */
+bool ascii_cmp(char const* s, char const* ascii) noexcept;
 
 } // namespace sds
--- src/string.cpp
+++ src/string.cpp
@@ -1,6 +1,7 @@
 #include "sds/string.h"
 
-int sds::byte_size(char const* s) noexcept {
+int sds::byte_size(char const* s) noexcept
+{
     if (!s) { return 0; }
 
     char const* p = s;
@@ -10,4 +11,29 @@ int sds::byte_size(char const* s) noexcept {
         ++len;
     }
     return len;
+}
+
+bool sds::ascii_cmp(char const* s, char const* ascii, s32 len) noexcept
+{
+    SDS_ASSERT(s);
+    SDS_ASSERT(ascii);
+    SDS_ASSERT(len > 0);
+
+    char const* p1 = s;
+    char const* p2 = ascii;
+    s32 n = 0;
+    while (n < len) {
+        SDS_ASSERT(is_ascii(*p2));
+        if (*p1 == '\0' || *p2 == '\0' || *p1 != *p2) { return false; }
+        ++n;
+        ++p1;
+        ++p2;
+    }
+
+    return true;
+}
+
+bool sds::ascii_cmp(char const* s, char const* ascii) noexcept
+{
+    return ascii_cmp(s, ascii, sds::str_size(ascii));
 }
