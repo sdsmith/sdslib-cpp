3ff50235669b547d543d0c85f90351d6e31ecb84
Stewart Smith <stewart.dryden.smith@gmail.com>
2021-04-27 21:29:16 -0400

    add lib config

---
+++ include/sds/config.h
@@ -0,0 +1,14 @@
+/*
+ * \file config.h
+ * \brief Library configuration.
+ */
+
+#ifndef SDS_USE_RTTI_FEATURES
+/*
+ * \def SDS_USE_RTTI_FEATURES
+ * \brief Enable runtime type info (RTTI) dependent parts of the library.
+ *
+ * Expensive. Off by default.
+ */
+#define SDS_USE_RTTI_FEATURES 0
+#endif
