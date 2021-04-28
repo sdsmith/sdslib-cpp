88b5e983ec90c730d6b6a30b010675767cb18995
Stewart Smith <stewart.dryden.smith@gmail.com>
2021-04-27 21:43:19 -0400

    add type_name for printing types

--- include/sds/type_traits.h
+++ include/sds/type_traits.h
@@ -1,15 +1,20 @@
 #pragma once
 
+#include "sds/details/common.h"
 #include <type_traits>
 
-namespace sds {
+#if SDS_USE_RTTI_FEATURES
+#    include <typeinfo>
+#endif
 
+namespace sds
+{
 /**
  * \brief Check if type \a T is contained in the type list \a Ts.
  *
  * Usage: `contains<int, Args...>::value` or `contains<int, char, bool, int>::value`.
  */
-template<typename T, typename... Ts>
+template <typename T, typename... Ts>
 struct contains : std::disjunction<std::is_same<T, Ts>...> {};
 
 /**
@@ -17,6 +22,16 @@ struct contains : std::disjunction<std::is_same<T, Ts>...> {};
  *
  * \see contains
  */
-template<typename T>
+template <typename T>
 inline constexpr bool contains_v = contains<T>::value;
+
+#if SDS_USE_RTTI_FEATURES
+template <typename T>
+char const* type_name()
+{
+    return typeid(T).name();
+}
+
+#    define SDS_TYPE_NAME(x) type_name<decltype(x)>()
+#endif
 } // namespace sds
