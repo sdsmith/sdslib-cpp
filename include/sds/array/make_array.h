8f7d958497fdbcd68e59b65988bb343b4e207eda
Stewart Smith <stewart.dryden.smith@gmail.com>
2023-04-28 14:43:25 -0700

    Support string literals in make_array

--- include/sds/array/make_array.h
+++ include/sds/array/make_array.h
@@ -10,12 +10,41 @@ namespace sds
  *
  * Avoids having to specify the number of elements in the array.
  *
+ * Usage:
+ *  auto arr = make_array(1, 2, 3);
+ *  auto arr = make_array<int>(1, 2, 3);
+ *
  * ref: https://gist.github.com/klmr/2775736#file-make_array-hpp
  */
 template <typename... T>
 constexpr auto make_array(T&&... values)
-    -> std::array<typename std::decay<typename std::common_type<T...>::type>::type, sizeof...(T)>
+    -> std::array<typename std::decay_t<typename std::common_type<T...>::type>, sizeof...(T)>
 {
     return {std::forward<T>(values)...};
 }
+
+// Helper
+template <typename T, std::size_t N, std::size_t ... Is>
+constexpr std::array<T, N>
+make_array(const T (&a)[N], std::index_sequence<Is...>)
+{
+    return {{a[Is]...}};
+}
+/**
+ * \brief Make \a str::array from string literals.
+ *
+ * Avoids having to specify the number of elements in the array.
+ *
+ * Usage:
+ *  auto arr = make_array("hello world")
+ *  auto arr = make_array<char>("hello world")
+ *
+ * ref: https://stackoverflow.com/a/33484394/3693388
+ */
+template <typename T, std::size_t N>
+constexpr std::array<T, N> make_array(const T (&a)[N])
+{
+    return make_array(a, std::make_index_sequence<N - 1>());
+}
+
 } // namespace sds
--- test/array/make_array_test.cpp
+++ test/array/make_array_test.cpp
@@ -2,7 +2,7 @@
 
 #include "sds/array/make_array.h"
 
-TEST(Make_Array_Test, constructor) {
+TEST(Make_Array_Test, vargs_constructor) {
     // NOTE(sdsmith): Not valid to create an empty array
     // auto a = sds::make_array<int>();
 
@@ -15,4 +15,28 @@ TEST(Make_Array_Test, constructor) {
     EXPECT_EQ(a2.size(), 3U);
     EXPECT_EQ(a2.front(), 1);
     EXPECT_EQ(a2.back(), 3);
+}
+
+TEST(Make_Array_Test, string_literal_constructor) {
+    // String literal overload
+
+    auto b1 = sds::make_array("hello world");
+    EXPECT_EQ(b1.size(), 12);
+    EXPECT_EQ(b1.front(), 'h');
+    EXPECT_EQ(b1.back(), '\0');
+
+    auto b2 = sds::make_array<char>("hello world");
+    EXPECT_EQ(b2.size(), 12);
+    EXPECT_EQ(b2.front(), 'h');
+    EXPECT_EQ(b2.back(), '\0');
+
+    auto b3 = sds::make_array(L"hello world");
+    EXPECT_EQ(b3.size(), 12);
+    EXPECT_EQ(b3.front(), 'h');
+    EXPECT_EQ(b3.back(), '\0');
+
+    auto b4 = sds::make_array<wchar_t>(L"hello world");
+    EXPECT_EQ(b4.size(), 12);
+    EXPECT_EQ(b4.front(), 'h');
+    EXPECT_EQ(b4.back(), '\0');
 }
